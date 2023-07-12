6fe379d5f97d2e4ea4c65fd355057c6e5e3de9ad
Sergey Pyzhuk <spyzhuk@gmail.com>
2023-07-12 15:13:59 +0300

    added Homework_3

---
+++ Homework_3/Task_19/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool validateValue (int value)
+{
+    string input = Convert.ToString(value);
+    if (input.Length >5 || input.Length <5)
+    {
+        Console.Write("Ошибка. Введено не пятизначное число.");
+        return false;
+    }
+    return true;
+}
+
+int reverseValue (int value)
+{ 
+    int remainder = 0;
+    int reversed = 0;
+    while (value!=0)
+    {
+        remainder = value%10;
+        reversed = reversed*10 +remainder;
+        value = value/10;
+    }
+        return reversed;
+}
+ 
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (validateValue(number))
+{
+    if (reverseValue(number) == number) Console.Write("Введенное число является палиндромом.");
+    else Console.Write("Введенное число НЕ является палиндромом.");
+}
+
---
+++ Homework_3/Task_21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int enterValue (string prompt)
+{
+    Console.Write($"Введите координату точки {prompt}");
+    int value = Convert.ToInt32(Console.ReadLine());
+    
+    return value;
+}
+
+int xA = enterValue("A по оси x: "),
+    yA = enterValue("A по оси y: "),
+    zA = enterValue("A по оси z: ");
+
+Console.WriteLine();
+
+int xB = enterValue("B по оси x: "),
+    yB = enterValue("B по оси y: "),
+    zB = enterValue("B по оси z: ");
+
+Console.WriteLine();
+
+double distance = Math.Sqrt(Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2) + Math.Pow((zA - zB),2));
+Console.Write($"Расстояние между точками равно: {Math.Round(distance,2)}");
