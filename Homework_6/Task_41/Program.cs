326f5d761597e385eb9d09830ce3a65d5a69f03d
Sergey Pyzhuk <spyzhuk@gmail.com>
2023-07-23 13:12:33 +0300

    added Homework_6

---
+++ Homework_6/Task_41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for(int i = 0; i<array.Length; i++)
+    {  
+        Console.Write($" число {i+1}: ");
+        array[i]= Convert.ToInt32(Console.ReadLine());  
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Введено {array.Length} чисел: [{string.Join(", ", array)}]");
+    return array;
+}
+void CheckPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(array[i]>0) count++;
+    }
+    Console.Write($"Среди введенных чисел {count} больше нуля.");
+}
+
+Console.Write("Задайте количество вводимых чисел: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Введите числа: ");
+int[] array = FillArray(length);
+CheckPositive(array);
---
+++ Homework_6/Task_43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetUserValue(string prompt)
+{
+    Console.Write($"Введите для прямой {prompt}");
+    double userValue = Convert.ToDouble(Console.ReadLine());
+    return userValue;
+}
+void Intersection(double b1, double k1, double b2, double k2)
+{ 
+    if (k1 == k2) // Равные значения k в любом случае делают прямые непересекающимися.
+    {
+        Console.Write("\nЗаданные прямые не пересекаются.\n");
+    }
+    else
+    {
+        double x = (b2-b1)/(k1-k2);
+        double y = k1*x + b1;
+        Console.Write($"\nТочка пересечения заданных прямых: ({x:0.0}; {y:0.0})\n");
+    }
+}
+double b1 = GetUserValue("1 значение b1: "),
+       k1 = GetUserValue("1 значение k1: "),
+       b2 = GetUserValue("2 значение b2: "),
+       k2 = GetUserValue("2 значение k2: ");
+
+Intersection(b1,k1,b2,k2);
